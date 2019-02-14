08f543364eb6190f1b438ee2e10427b47578d38c
Josh Hug <josh@joshh.ug>
2019-02-14 10:03:14 -0800

    starter code for inheritance4 (lecture 11

---
+++ exercises/DIY/inheritance4/ArraySet.java
@@ -0,0 +1,39 @@
+public class ArraySet<T> {
+
+    public ArraySet() {
+    }
+
+    /* Returns true if this map contains a mapping for the specified key.
+     */
+    public boolean contains(T x) {
+        return false;
+    }
+
+    /* Associates the specified value with the specified key in this map. 
+       Throws an IllegalArgumentException if the key is null. */
+    public void add(T x) {
+        return;
+    }
+
+    /* Returns the number of key-value mappings in this map. */
+    public int size() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        ArraySet<String> s = new ArraySet<>();
+        s.add(null);
+        s.add("horse");
+        s.add("fish");
+        s.add("house");
+        s.add("fish");        
+        System.out.println(s.contains("horse"));        
+        System.out.println(s.size());       
+    }
+
+    /* Also to do:
+    1. Implement a toString method.
+    2. Implement an equals() method.
+    3. Make ArraySet implement the Iterable<T> interface.
+    */
+}
